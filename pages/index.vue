549a82103abec9fb57d5e57286cb87d9ec296a81
Nikola Pavicevic <nijepa@gmail.com>
2023-10-27 20:09:53 +0200

    front posts added

--- pages/index.vue
+++ pages/index.vue
@@ -28,13 +28,18 @@
   <section>
     <h2 class="text-3xl font-bold mt-8">Latest Blog Posts</h2>
     <div class="grid md:grid-cols-3 pt-8 gap-10">
-      <!-- <Post :posts="posts" /> -->
+      <Post :posts="posts" />
     </div>
   </section>
 </template>
 
 <script setup>
-
+const { data: posts } = await useAsyncData('latest-posts', () =>
+  queryContent('/blog')
+    .sort({ data: 1 })
+    .limit(3)
+    .find()
+)
 </script>
 
 <style lang="scss" scoped>
