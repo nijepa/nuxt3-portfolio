dd730d9d264c65d2a7446a21f9eb80a4f0814f67
Nikola Pavicevic <nijepa@gmail.com>
2023-10-29 10:41:35 +0100

    tags added

---
+++ pages/blog/tags/[...slug].vue
@@ -0,0 +1,18 @@
+<script setup>
+const { path } = useRoute()
+
+const { data } = await useAsyncData(`content-${path}`, () => {
+  return queryContent()
+    .where({ _path: path })
+    .findOne()
+})
+console.log(9, path)
+</script>
+
+<template>
+  <ContentRenderer :value="data" class="prose my-10 mx-auto max-w-7xl">
+    <template #empty>
+        <p>No content found.</p>
+      </template>
+  </ContentRenderer>
+</template>
---
+++ pages/blog/tags/index.vue
@@ -0,0 +1,15 @@
+<script setup>
+// const { data: posts } = await useAsyncData('posts', () =>
+//   queryContent('/blog').find()
+// )
+</script>
+
+<template>
+  <h1 class="text-3xl my-8">Tags</h1>
+
+  tags
+
+  <section class="grid md:grid-cols-3 mt-8 gap-10">
+    <!-- <Post :posts="posts" /> -->
+  </section>
+</template>
